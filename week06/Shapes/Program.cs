dd4c860d461e4e29ac4c54acf01ab5450ee2c19d
bright-droid <ndu24006@byui.edu>
2025-06-19 21:38:30 +0100

    Shapes program completed

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.ObjectModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("blue", 4);
+        shapes.Add(square);
+
+        Rectangle rect = new Rectangle("red", 5, 8);
+        shapes.Add(rect);
+
+        Circle circle = new Circle("yellow", 6);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shapre has an area of {area}.");
+        }
     }
 }
