980f1c875c2b92ed8e3226d1b9b763c213f42aa0
bright-droid <bndubuisi@byupathway.edu>
2026-07-08 01:31:37 +0100

    Exercise 4 completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Globalization;
+using System.Runtime.InteropServices;
 using System.Transactions;
 
 class Program
@@ -9,27 +10,55 @@ class Program
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
 
         List<int> numbers = new List<int>();
-        int number = -1;
+        int userNumber = -1;
 
-        while (number != 0)
+        while (userNumber != 0)
         {
             Console.Write("Enter number: ");
-            number = int.Parse(Console.ReadLine());
+            userNumber = int.Parse(Console.ReadLine());
 
-            if (number != 0)
+            if (userNumber != 0)
             {
-                numbers.Add(number);                
+                numbers.Add(userNumber);
             }
         }
-        
-        int sum = numbers.Sum();
+
+        // alternatively using Sum() function in c#
+        // int sum = numbers.Sum();
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
         Console.WriteLine($"The sum is: {sum}");
 
-        double average = numbers.Average();
+        // alternatively using Average() funtion in c#
+        // double average = numbers.Average();
+
+        // By making one of the variables a float first, 
+        // the computer knows that it has to do the floating point division, 
+        // and we get the decimal value that we expect.
+
+        // So we first cast the sum variable to be a float.Otherwise, because
+        // both the sum and the count are integers, the computer will do 
+        // integer division and I will not get a decimal value
+        float average = ((float)sum) / numbers.Count;
         Console.WriteLine($"The average is: {average}");
 
-        int largest = numbers.Max();
-        Console.WriteLine($"The largest number is: {largest}");
+        //alternatively using Max() function in c#
+        //int max = numbers.Max();
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                //if this number is greater than the max, we have found
+                //the new max!
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
 
     }
 }
